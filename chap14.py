791d703c99c7bfaf378b6ae290db351ec79db571
itraa <itraa.narayan@gmail.com>
2013-09-11 20:53:42 -0400

    Dev-sprint6 submission

--- chap14.py
+++ chap14.py
@@ -1,1 +1,17 @@
 # This is where chapter 14 exercises go.
+import urllib
+#from HTMLParser import HTMLParser
+
+fout = open('output.txt', 'w')
+param = raw_input("Enter zip code: ")
+conn = urllib.urlopen("http://www.uszip.com/zip/%s" % param)
+fout.write(conn.read())
+fp = open('output.txt')
+for line in fp:
+    line = line.strip()
+    if 'Population' in line:
+        print line
+fp.close()
+
+#for line in conn:
+#    line.strip()
